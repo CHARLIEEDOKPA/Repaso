5f08d4b382f54b0bbf4eb36d8a7ca236a5f36f1e
Charlie Onosetale <jnr.31gcharlieedokpa@gmail.com>
2023-02-21 09:26:13 +0100

    Hice una clase llamado Calculadora

---
+++ src/ejemplo/Calculadora.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejemplo;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Calculadora {
+
+    private int a;
+    private int b;
+
+    public Calculadora(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int sumar() {
+        return a + b;
+    }
+
+}
